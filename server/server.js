23f3df72bba982e52d055a88cc64349cb43bee0c
Dipeshagrwal <dipeshagrawal43@gmail.com>
2025-11-14 16:25:41 +0530

    Product Manager app work Successfully

--- server/server.js
+++ server/server.js
@@ -14,9 +14,12 @@ app.use(cors({
   origin: [
     'http://localhost:3000', 
     'http://127.0.0.1:3000',
-    'https://product-manager-gray.vercel.app/'  // This allows ANY Vercel deployment
+    'https://product-manager-gray.vercel.app',
+    'https://*.vercel.app'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Middleware
